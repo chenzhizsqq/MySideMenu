0b9231bfd1a547d3ad7ae754545611ce27337c81
chenzhizs <chenzhi@qq.com>
2021-04-19 17:23:08 +0900

    看到一半，暂时了解这个框架。感觉教程很多突然变化，所以不想再看了

--- MySideMenu/ViewController.swift
+++ MySideMenu/ViewController.swift
@@ -10,7 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private let sideMenu = SideMenuNavigationController(rootViewController: UIViewController())
+    private let sideMenu = SideMenuNavigationController(rootViewController: MenuController(with: [
+        "Home",
+        "Info",
+        "Setting",
+    ]))
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,3 +31,44 @@ class ViewController: UIViewController {
     }
 }
 
+class MenuController: UITableViewController {
+    private let menuItems: [String]
+    private let color = UIColor(red: 33/255.0, green: 33/255.0, blue: 33/255.0, alpha: 1)
+    
+    init(with menuItems:[String]) {
+        self.menuItems = menuItems
+        super.init(nibName: nil, bundle: nil)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init error")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.backgroundColor = color
+        view.backgroundColor = color
+    }
+    
+    // Table
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menuItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell" , for: indexPath)
+        cell.textLabel?.text = menuItems[indexPath.row]
+        cell.textLabel?.textColor = .white
+        cell.backgroundColor = color
+        cell.contentView.backgroundColor = color
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
