6ec35bdff339945642e2d824af77325d2cbe5481
chenzhizs <chenzhi@qq.com>
2021-04-19 16:29:12 +0900

    sideMenu的基本框架完成

--- MySideMenu/ViewController.swift
+++ MySideMenu/ViewController.swift
@@ -5,15 +5,25 @@
 //  Created by chenzhizs on 2021/04/19.
 //
 
+import SideMenu
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let sideMenu = SideMenuNavigationController(rootViewController: UIViewController())
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        sideMenu.leftSide = true
+        SideMenuManager.default.leftMenuNavigationController = sideMenu
+        SideMenuManager.default.addPanGestureToPresent(toView: view)
     }
 
 
+    @IBAction func didTapMenuButton() {
+        present(sideMenu, animated: true)
+    }
 }
 
